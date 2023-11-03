bdc884270795134a0b028c0c4dd2625214f87bf9
jishan1019 <imranislamjishan200@gmail.com>
2023-11-03 19:53:00 +0600

    data type declear

---
+++ javascript/dist/dataType.js
@@ -0,0 +1,14 @@
+"use strict";
+// String
+let fristName = "Jishan Hossain";
+let roll = 10;
+let isAdmin = true;
+let x = undefined;
+let y = null;
+let d;
+d = 123;
+// Array
+let friends = ["a", "b", "c", "d"];
+let eligiblrRollList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+eligiblrRollList.push(d);
+console.log(eligiblrRollList);
---
+++ typescript/src/dataType.ts
@@ -0,0 +1,20 @@
+// String
+
+let fristName: string = "Jishan Hossain"
+let roll: number = 10
+let isAdmin: boolean = true
+let x: undefined = undefined
+let y: null = null
+
+let d : number;
+d = 123
+
+
+// Array
+let friends: [string, number] = ["a", 7]
+
+let eligiblrRollList: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+eligiblrRollList.push(d);
+
+console.log(eligiblrRollList);
+
